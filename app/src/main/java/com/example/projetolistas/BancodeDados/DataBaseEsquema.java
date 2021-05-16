a3fdef344667c6a5692dece6ec6f1a5e2bcf94d4
Giulia <giuliasoaresrs@gmail.com>
2021-05-15 21:19:49 -0300

    iniciando a branch

---
+++ app/src/main/java/com/example/projetolistas/BancodeDados/DataBaseEsquema.java
@@ -0,0 +1,25 @@
+package com.example.projetolistas.BancodeDados;
+
+import android.database.sqlite.SQLiteDatabase;
+
+public class DataBaseEsquema {
+
+    public class CriaBanco extends DataBaseEsquema
+    {
+        //@Override
+
+        public void onCreate(SQLiteDatabase db)
+        {
+
+        }
+    }
+    //@Override
+
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion)
+    {
+
+
+    }
+
+
+}
