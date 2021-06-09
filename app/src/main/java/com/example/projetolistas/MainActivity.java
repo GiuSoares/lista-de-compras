b5910b55fb79bb3ecc45042010b836a752dcf2cd
Mário Henrique <marioh90@gmail.com>
2021-06-09 09:02:14 -0300

    Correção de elementos no XML e refatoração de código

--- app/src/main/java/com/example/projetolistas/MainActivity.java
+++ app/src/main/java/com/example/projetolistas/MainActivity.java
@@ -1,69 +1,61 @@
 package com.example.projetolistas;
+
 import android.app.Activity;
 import android.content.Intent;
-import android.support.design.widget.FloatingActionButton;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.ListView;
 
 import com.example.projetolistas.JavaClasses.Produto;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 import java.util.List;
 
 public class MainActivity extends Activity {
+	private FloatingActionButton addLista;
 
-    private FloatingActionButton addLista;
+	ListView MinhasListas;
+	List<Produto> Lista;
+	AdapterProduto adapter;
 
-    ListView MinhasListas;
-    List<Produto> Lista;
-    AdapterProduto adapter;
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
 
+		Log.i("adalto", "oi Zé Luis");
 
+		addLista = (FloatingActionButton) findViewById(R.id.addLista);
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+		MinhasListas = (ListView) findViewById(R.id.minhasListas);
 
-        Log.i("adalto","oi Zé Luis");
+		addLista.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				startActivity(new Intent(MainActivity.this, AddprodutoActivity.class));
+			}
+		});
+	}
 
-        addLista = (FloatingActionButton) findViewById(R.id.addLista);
+	private void carregarLista() {
+		Log.i("adalto", "oi Zé");
 
-        MinhasListas = (ListView)findViewById(R.id.minhasListas);
+		Lista = ProdutoDAO.listar(this);
 
-        addLista.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-                startActivity(new Intent(MainActivity.this, AddprodutoActivity.class));
-
-            }
-        });
-
-
-    }
-
-    private void carregarLista(){
-
-        Log.i("adalto","oi Zé");
-
-        Lista = ProdutoDAO.listar(this);
-
-        Log.i("cont","total: " + Lista.size());
+		Log.i("cont", "total: " + Lista.size());
 
 //manter        adapter = new ArrayAdapter(this,
 //manter        android.R.layout.simple_list_item_1, lista );
-        adapter = new AdapterProduto(this, Lista);
-        MinhasListas.setAdapter(adapter);
-    }
+		adapter = new AdapterProduto(this, Lista);
+		MinhasListas.setAdapter(adapter);
+	}
 
-    @Override
-    protected void onResume() {
-        super.onResume();
-        carregarLista();
-    }
-
+	@Override
+	protected void onResume() {
+		super.onResume();
+		carregarLista();
+	}
 
 
 }
